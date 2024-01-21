10105ee093bab3572fe4cee3c3da4af2011c51ce
juan azocar malverde <jazocar@gasco.cl>
2024-01-21 13:38:22 -0300

    mensaje

--- js/format.js
+++ js/format.js
@@ -1,32 +1,33 @@
+$('#head-of-document').html('<meta charset="UTF-8">'+
+  '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
+  '<title>Obten empleo</title>'+
+  '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">'+
+  '<link rel="stylesheet" href="css/style.css">'+
+  '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css" crossorigin="anonymous" />');
+
 
 $('#footer').html('<div class="row"><div class="col-3 navbar-dark bg-dark nav-border-top" style="padding: 20px 0px 20px 30px;">'+
     '<img src="img/image.png" class="circle-image-footer" ></img>'+
     '</div>'+
     '<div class="col-2 navbar-dark bg-dark  nav-border-top" style="padding: 20px 0px 20px 0px;">'+
     '<h6 style="color:white;">General</h6>'+
-    '<a href="#" class="footer-text-links">Regístrate</a><br/>'+
-    '<a href="#" class="footer-text-links">Centro de ayuda</a><br/>'+
-    '<a href="#" class="footer-text-links">Acerca de</a><br/>'+
-    '<a href="#" class="footer-text-links">Empleos</a><br/>'+
-    '<a href="#" class="footer-text-links">Desarrolladores</a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:small;">Regístrate</a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:small;">Centro de ayuda</a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:small;">Acerca de</a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:small;">Empleos</a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:small;">Desarrolladores</a><br/>'+
     '</div>'+
     '<div class="col-2 navbar-dark bg-dark  nav-border-top" style="padding: 20px 0px 20px 0px;">'+
     '<h6 style="color:white;">Links de interés</h6>'+
-    '<a href="#" class="footer-text-links">1</a><br/>'+
-    '<a href="#" class="footer-text-links">2</a><br/>'+
-    '<a href="#" class="footer-text-links">3</a><br/>'+
-    '<a href="#" class="footer-text-links">4</a><br/>'+
-    '<a href="#" class="footer-text-links">5</a><br/>'+
-    '<a href="#" class="footer-text-links">6</a><br/>'+
-    '<a href="#" class="footer-text-links">7</a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:small;">1</a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:small;">2</a><br/>'+
     '</div>'+
     '<div class="col-2 navbar-dark bg-dark  nav-border-top" style="padding: 20px 0px 20px 0px;">'+
     '<h6 style="color:white;">Redes Sociales</h6>'+
-    '<a href="#" class="footer-text-links">1</a><br/>'+
-    '<a href="#" class="footer-text-links">2</a><br/>'+
-    '<a href="#" class="footer-text-links">3</a><br/>'+
-    '<a href="#" class="footer-text-links">4</a><br/>'+
-    '<a href="#" class="footer-text-links">5</a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:x-large;"><i class="fa-brands fa-youtube" style="width:90px;" aria-hidden="true"></i> </a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:x-large;"><i class="fa-brands fa-instagram" style="width:90px;" aria-hidden="true"></i></a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:x-large;"><i class="fa-brands fa-threads" style="width:90px;" aria-hidden="true"></i></a><br/>'+
+    '<a href="#" class="footer-text-links" style="font-size:x-large;"><i class="fa-brands fa-x-twitter" style="width:90px;" aria-hidden="true"></i></a><br/>'+
     '</div>'+
     '<div class="col-3 navbar-dark bg-dark  nav-border-top" style="padding: 20px 0px 20px 0px;">'+
     '</div>'+
